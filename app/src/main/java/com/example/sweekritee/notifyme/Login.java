3ad75e4f38c7438996bd8048dce1c0236877d39a
SwikritiT <Swikriti808@gmail.com>
2018-08-06 14:54:36 +0545

    changed

--- app/src/main/java/com/example/sweekritee/notifyme/Login.java
+++ app/src/main/java/com/example/sweekritee/notifyme/Login.java
@@ -10,14 +10,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.example.sweekritee.notifyme.R.id.username_login;
 
 
 public class Login extends AppCompatActivity {
     Button loginbtn;
     String a;
-    TextView textView1;
+    TextView textView1, textView2;
     EditText user, pass;
+    String reg_url = "http://192.168.1.12/notifyme/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +44,16 @@ public class Login extends AppCompatActivity {
 
 
         textView1 = (TextView) findViewById(R.id.extratext);
-        final int flag = Integer.parseInt(getIntent().getExtras().getString("extratext"));
-        if(flag == 1)
-            textView1.setText("For Admin");
-        else if(flag == 0)
-        textView1.setText("For Students");
+        textView2=(TextView) findViewById(R.id.register);
+        final int f = Integer.parseInt(getIntent().getExtras().getString("extratext"));
+        //final int flag2 = Integer.parseInt(getIntent().getExtras().getString("extratext2"));
+        if(f == 1)
+            textView1.setText(R.string.fadmin);
+        else if(f == 0 )
+        {
+        textView1.setText(R.string.fstudents);
+        textView2.setText(R.string.noAccount);
+        }
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,23 +61,20 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 String username = user.getText().toString();
                 String password = pass.getText().toString();
-                if (username.length() >5 || password.length() > 5) {
-                    if (flag == 0) {
-                        Log.d("test",">>>>1"+textView1.getText());
-                        Intent intent = new Intent(Login.this, Student_homepage.class);
-                        startActivity(intent);
-                    }
-                    else if (flag == 1){
-                        Log.d("test",">>>>2"+textView1.getText());
-                        Intent intent = new Intent(Login.this, Admin_Homepage.class);
-                        startActivity(intent);
-                    }
 
-
-                }
-                else if (username.equals("") || password.equals("")) {
+                if (username.equals("") || password.equals("")) {
                     Toast.makeText(getBaseContext(), "Fill up all the field properly", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                else
+                {
+                    UserLoginFunction(username, password,f);
+
+
+
+                }
+
+
 
 
 
@@ -65,9 +82,89 @@ public class Login extends AppCompatActivity {
             }
         });
 
-    }
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
 
-        }
+            public void onClick(View v) {
+                Intent intent= new Intent(Login.this,SignUpStd.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+    }
+    public void UserLoginFunction(final String username,  final String password, final int f){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, reg_url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject= new JSONObject(response);
+                            String success =jsonObject.getString("flag");
+//                            JSONObject myObj=new JSONObject(success);
+                            if (success.equals("1")){
+                                if (f == 0) {
+                                    Log.d("test",">>>>1"+textView1.getText());
+                                    Toast.makeText(Login.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Login.this, Student_homepage.class);
+                                    startActivity(intent);
+                                }
+                                else if (f == 1){
+                                    Log.d("test",">>>>2"+textView1.getText());
+                                    Toast.makeText(Login.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Login.this, Admin_Homepage.class);
+                                    startActivity(intent);
+                                }
+
+                            }
+                            else
+                            {
+                                Toast.makeText(Login.this,"Enter correct username or password",Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(Login.this,"Login failed"+e.toString(),Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this,"Login Failed"+error.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params=new HashMap<>();
+                params.put("username",username);
+                params.put("password",password);
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
