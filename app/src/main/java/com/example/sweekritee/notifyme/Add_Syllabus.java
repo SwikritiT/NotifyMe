c20ce8babd98de119a78f42bb1ca451eea9f8309
SwikritiT <Swikriti808@gmail.com>
2018-08-06 14:50:20 +0545

    changed

--- app/src/main/java/com/example/sweekritee/notifyme/Add_Syllabus.java
+++ app/src/main/java/com/example/sweekritee/notifyme/Add_Syllabus.java
@@ -1,13 +1,25 @@
 package com.example.sweekritee.notifyme;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Add_Syllabus extends AppCompatActivity {
+    private FloatingActionButton goback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_syllabus);
+        goback= (FloatingActionButton) findViewById(R.id.FAB_addsyllabus);
+        goback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Add_Syllabus.this,Admin_Homepage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
