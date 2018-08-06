770583a1f547d9692c7190b22b8e3123f3f8f84e
SwikritiT <Swikriti808@gmail.com>
2018-08-06 14:55:17 +0545

    changed

--- app/src/main/java/com/example/sweekritee/notifyme/NavigationBarAdmin.java
+++ app/src/main/java/com/example/sweekritee/notifyme/NavigationBarAdmin.java
@@ -1,16 +1,38 @@
 package com.example.sweekritee.notifyme;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NavigationBarAdmin extends AppCompatActivity {
+    private DrawerLayout drawerLayoutadm;
+    private ActionBarDrawerToggle admToggle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigationbaradmin);
+        drawerLayoutadm=(DrawerLayout) findViewById(R.id.drawerlayout_ad);
+        admToggle=new ActionBarDrawerToggle(NavigationBarAdmin.this,drawerLayoutadm,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayoutadm.addDrawerListener(admToggle);
+        admToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (admToggle.onOptionsItemSelected(item)) {
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 }
