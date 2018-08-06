088daf4a4830c74b6d14e41eee6765b5cbc60506
SwikritiT <Swikriti808@gmail.com>
2018-08-06 14:53:42 +0545

    changed

--- app/src/main/java/com/example/sweekritee/notifyme/firstPage.java
+++ app/src/main/java/com/example/sweekritee/notifyme/firstPage.java
@@ -1,13 +1,24 @@
 package com.example.sweekritee.notifyme;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class firstPage extends AppCompatActivity {
+    private static final int SPLASH_TIME_OUT=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.firstPage);
+        setContentView(R.layout.firstpage);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(firstPage.this,AdmORstd.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
