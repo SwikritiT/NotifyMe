cad410d18e6d6f0c1bc17d3e080668bf0f37bf4d
SwikritiT <Swikriti808@gmail.com>
2018-08-06 14:51:03 +0545

    changed

--- app/src/main/java/com/example/sweekritee/notifyme/Admin_Homepage.java
+++ app/src/main/java/com/example/sweekritee/notifyme/Admin_Homepage.java
@@ -1,15 +1,23 @@
 package com.example.sweekritee.notifyme;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class Admin_Homepage extends AppCompatActivity {
     Toolbar toolbar1;
     Button addevent, addroutine, addsyllabus;
+    private DrawerLayout drawerLayoutadm;
+    private ActionBarDrawerToggle admToggle;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,10 @@ public class Admin_Homepage extends AppCompatActivity {
         setContentView(R.layout.admin_homepage);
         toolbar1=(Toolbar) findViewById(R.id.toolbar_ad);
         setSupportActionBar(toolbar1);
-        //getSupportActionBar().setIcon(getDrawable(R.drawable.menu_ad));
-       // toolbar1.setTitle("NotifyMe");
+        drawerLayoutadm=(DrawerLayout) findViewById(R.id.drawerlayout_ad);
+        admToggle=new ActionBarDrawerToggle(Admin_Homepage.this,drawerLayoutadm,toolbar1,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayoutadm.addDrawerListener(admToggle);
+        admToggle.syncState();
         addevent=(Button) findViewById(R.id.addevent_btn);
         addroutine=(Button) findViewById(R.id.addroutine_btn);
         addsyllabus=(Button) findViewById(R.id.addsyllabus_btn);
@@ -51,4 +61,22 @@ public class Admin_Homepage extends AppCompatActivity {
         });
 
 }
+    @Override
+    public void onBackPressed() {
+        if(drawerLayoutadm.isDrawerOpen(GravityCompat.START)){
+            drawerLayoutadm.closeDrawer(GravityCompat.START);
+
+        }
+        else{
+            super.onBackPressed();}
+    }
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (admToggle.onOptionsItemSelected(item)) {
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }*/
 }
