8d54c9d53e5d6a008a7332c807dd8aa47a4f1c45
SwikritiT <Swikriti808@gmail.com>
2018-08-06 14:59:01 +0545

    changed

--- app/src/main/java/com/example/sweekritee/notifyme/ViewSyllabus.java
+++ app/src/main/java/com/example/sweekritee/notifyme/ViewSyllabus.java
@@ -1,13 +1,20 @@
 package com.example.sweekritee.notifyme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ViewSyllabus extends AppCompatActivity {
+/**
+ * Created by SWEEKRITEE on 7/14/2018.
+ */
 
+public class ViewSyllabus extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.viewsyllabus);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.viewsyllabus,container,false);
     }
 }
